3b92237c9505aea6fa51e89fb922e35896b38c54
Kyle Martinelli <kylelmartinelli@gmail.com>
2022-10-31 12:10:21 -0700

    Created subclasses for all types of bees in ES6 format.

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,18 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor(age, job, canFly, color, forage) {
+    super(age, job, canFly, color)
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey'
+
+  }
+  forage() {
+    super.forage();
+    return 'I am too old, let me play cards instead';
+  }
+
+  gamble(treasure) {
+    this.treasureChest.push(treasure)
+  }
 };
